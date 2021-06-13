e34dc64a8f0035fcc3d01a9aae51cda0b8b85fcd
Marcos Iorio <marcossiorio@gmail.com>
2021-06-13 18:07:31 -0300

    solucionando problemas

--- app/index.php
+++ app/index.php
@@ -43,6 +43,11 @@ $app->group('/login', function (RouteCollectorProxy $group) {
     
 });
 
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
 $app->run();
   
 ?>
